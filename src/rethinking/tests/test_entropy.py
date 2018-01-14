4d244351d9bc301c73b66568628c202ff32aa6d3
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2018-01-14 17:00:38 -0500

    BUG: numpy array created using wrong syntax

--- src/rethinking/tests/test_entropy.py
+++ src/rethinking/tests/test_entropy.py
@@ -10,15 +10,13 @@ from rethinking.utils import (
     (np.array([.7, .15, .15]), 0.81880845),
     # according to l'hopital rule, 0 * log(0) = 0
     (np.array([.7, .15, .15, .0]), 0.81880845)  
-    
 ])
 def test_information_entropy(p, expected):
     assert pytest.approx(information_entropy(p=p), rel=1e-3) == expected
     
     
 @pytest.mark.parametrize('p,q,expected', [
-    (np.array([.3, .7]), np.array([.3, .7]), 0),
-    (np.array([.3, .7], np.array(.3, .7), 0))
+    (np.array([.3, .7]), np.array([.3, .7]), 0)
 ])
 def test_kl_divergence(p, q, expected):
     assert kl_divergence(p, q) == expected
